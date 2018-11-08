cb15c435cbc55c692036c0800d521a34c4ef2427
mrsmile <mr.smile_lp@mail.ru>
2018-11-09 00:41:25 +0300

    html done, get all sources done

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const apiKey = '7a67c40c89ea4952830387b8f9e5090e';
+const url = 'https://newsapi.org/v2/sources?apiKey=';
+const list = document.getElementById('list');
+
+fetch(url + apiKey)
+.then(function(response) {
+    return response.json();    
+}).then(function(data) {
+    data.sources.map(function(elem) {
+        var option = document.createElement('option');
+        var content = document.createTextNode(elem.name);
+        option.appendChild(content);
+        return list.appendChild(option); 
+    }).join('\n');
+});
